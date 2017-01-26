1376f6605d5f7e37435fb8f8b34d4024a980fca2
SunitaMore <sunitamore078@gmail.com>
2017-01-26 20:18:14 +0530

    program is added.

---
+++ DayMonthDemo.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class DayMonthDemo
+{
+	public static void main(String args[])
+	{
+		int num = Integer.parseInt(args[0]);
+		int days = num%30;
+		int month = num/30;
+		System.out.println(num+"days="+month+"month and"+days+"days");
+	}
+} 
